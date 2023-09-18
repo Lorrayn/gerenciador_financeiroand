7b847ab64b78605c3409f6632efddabc7325ac6b
lorrayne <o.miranda@aluno.ifsp.edu.br>
2023-09-18 11:40:08 -0300

    Crud

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// app.listen(3000, ()=> console.log("Deu baum!"));
+// app.listen(3000, ()=> console.log("Deu certo!"));
 
 app.use(express.json())
 
@@ -10,7 +10,7 @@ app.use(express.json())
 //     res.send("Bem-vindo(a) à nossa primeira aplicação. ola")
 // });
 
-// const dados = ["Eu tenho mais coxa que a Coppi"];
+// const dados = ["Eu legal"];
 
 // app.get('/j', (req, res)=> {
 //     res.json({dados})
@@ -36,5 +36,50 @@ const getAllPessoas = async() => {
 
 app.get('/pessoa', async(req, res) => {
     const resultado = await getAllPessoas()
-    return res.status(200).json(resultado)
+    return res.status(200).json(resultado)/*responde pro usuario*/
+
 });
+
+app.get('/pessoa/:id', async (req,res)=>{
+    const {id} = req.params;
+    const [query] = await conection.execute('select * from pessoa where id = ?', [id]);
+    if (query.length===0) return res.status(400).json({mensagem: 'nenhuma pessoa encontrada'});
+    return res.status(200).json(query)
+
+}) 
+
+app.get('/pessoa/buscarnome/:nome', async (req,res)=>{
+    const {nome} = req.params;
+    const [query] = await conection.execute('select * from pessoa where nome like ?', ['%'+nome+'%']);
+    if (query.length===0) return res.status(400).json({mensagem: 'nenhuma pessoa encontrada'});
+    return res.status(200).json(query)
+
+}) 
+app.get('/pessoa/buscaremail/:email', async (req,res)=>{
+    const {email} = req.params;
+    const [query] = await conection.execute('select * from pessoa where email like ?', ['%'+email+'%']);
+    if (query.length===0) return res.status(400).json({mensagem: 'nenhuma pessoa encontrada'});
+    return res.status(200).json(query)
+
+}) 
+
+app.post('/pessoa', async (req,res)=>{
+    const {nome, email} = req.body;
+    const [query] = await conection.execute('insert into pessoa (nome, email) values (?, ?)',[nome, email]);
+    return res.json(query);
+})
+app.put('/pessoa/:id', async (req,res)=>{ /*atualizações do q ja existe*/
+    const {id} = req.params;
+    const {nome, email} = req.body;
+
+    const [query] = await conection
+    .execute('update pessoa set nome = ?, email = ? where id = ?',[nome, email, id]);
+    return res.json(query);
+})
+app.delete('/pessoa/:id', async(req,res) =>{
+    const {id} = req.params
+    const [query] = await conection.execute('delete from pessoa where id = ?', [id])
+    return res.json(query)
+})
+
+
